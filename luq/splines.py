d49cf9e5844d8cf044bcb7ea59ab0983b2268df8
Steve Mattis <steve.a.mattis@gmail.com>
2020-03-02 14:36:54 -0500

    updates cleaning to stop spurious problems

--- luq/splines.py
+++ luq/splines.py
@@ -43,7 +43,6 @@ def linear_c0_spline(times, data, num_knots, clean_times, spline_old=None, verbo
         knots = np.append(knots, times[-1])
         return np.interp(x, knots, Qs)
     
-    knots_init = np.linspace(0, 1, num_knots)[1:-1]
     if spline_old is None:
         vals_init = np.zeros((num_knots,))
     else:
@@ -57,33 +56,52 @@ def linear_c0_spline(times, data, num_knots, clean_times, spline_old=None, verbo
             param_bounds[:, i] = [-np.inf, np.inf]
         else:
             param_bounds[:, i] = [0, 1]
+
+    knots_unif = np.linspace(0, 1, num_knots)[1:-1]
+    q_pl_unif, _ = optimize.curve_fit(lambda x, *params_0: wrapper_fit_func_qs(x, knots_unif, params_0),
+                                 (times - times[0]) / (times[-1] - times[0]),
+                                 data,
+                                 p0=vals_init)
+    q_pl_unif = np.hstack([q_pl_unif, knots_unif])
+    q_pl_unif[num_knots:] *= (times[-1] - times[0])
+    q_pl_unif[num_knots:] += times[0]
+
+    clean_data_at_original_unif = piecewise_linear_clean(times,
+                                                         q_pl_unif[num_knots:],
+                                                         q_pl_unif[0:num_knots])
+    ssr_unif = np.sum((data - clean_data_at_original_unif)**2)
      
     # find piecewise linear splines for predictions
+    opt_fail = False
     try:
+        knots_init = np.linspace(0, 1, num_knots)[1:-1]
         q_pl, _ = optimize.curve_fit(lambda x, *params_0: wrapper_fit_func(x, num_knots, params_0),
                                      (times-times[0])/(times[-1]-times[0]),
                                      data,
-                                     p0=np.hstack([vals_init, knots_init]),
+                                     p0=np.hstack([q_pl_unif[0:num_knots], knots_init]),
                                      bounds=param_bounds,
                                      verbose=verbose)
+        q_pl[num_knots:] *= (times[-1] - times[0])
+        q_pl[num_knots:] += times[0]
+
+        # fix if knots get out of order
+        inds_sort = np.argsort(q_pl[num_knots:])
+        q_pl[1:num_knots - 1] = q_pl[inds_sort + 1]  # Qs
+        q_pl[num_knots:] = q_pl[inds_sort + num_knots]  # knots
+
+        clean_data_at_original = piecewise_linear_clean(times,
+                                                             q_pl[num_knots:],
+                                                             q_pl[0:num_knots])
+        ssr = np.sum((data - clean_data_at_original) ** 2)
 
     except RuntimeError:
         # Use uniform knots if optimization fails
+        opt_fail = True
+
+    if opt_fail or ssr_unif < ssr:
         print('Optimization of knot locations failed. Using uniform knots.')
-        knots = np.linspace(0, 1, num_knots)[1:-1]
-        q_pl, _ = optimize.curve_fit(lambda x, *params_0: wrapper_fit_func_qs(x, knots, params_0),
-                                     (times-times[0])/(times[-1]-times[0]),
-                                     data,
-                                     p0=vals_init)
-        q_pl = np.hstack([q_pl, knots])
-
-    q_pl[num_knots:] *= (times[-1]-times[0])
-    q_pl[num_knots:] += times[0]
-
-    # fix if knots get out of order
-    inds_sort = np.argsort(q_pl[num_knots:])
-    q_pl[1:num_knots-1] = q_pl[inds_sort+1]  # Qs
-    q_pl[num_knots:] = q_pl[inds_sort + num_knots]  # knots
+        q_pl = q_pl_unif
+        clean_data_at_original = clean_data_at_original_unif
 
     # calculate clean data
     clean_data = piecewise_linear_clean(clean_times,
@@ -91,10 +109,6 @@ def linear_c0_spline(times, data, num_knots, clean_times, spline_old=None, verbo
                                         q_pl[0:num_knots])
 
     # calculate mean absolute error between spline and original data
-    clean_data_at_original = piecewise_linear_clean(times,
-                                                    q_pl[num_knots:],
-                                                    q_pl[0:num_knots])
-    
     error = np.average(np.abs(clean_data_at_original - data))
     error = error / np.average(np.abs(data))
     return clean_data, error, q_pl
